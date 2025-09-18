b68fc8ce90be838023efdde4f1b56c0abb30652b
dayano <kiixxn00@gmail.com>
2025-09-18 17:05:53 +0900

    add ex00

---
+++ ex00/srcs/ClapTrap.cpp
@@ -0,0 +1,12 @@
+#include "ClapTrap.hpp"
+
+#include <iostream>
+
+ClapTrap::ClapTrap(const std::string& name)
+    : name(name), hitPoints(10), energyPoints(10), attackDamage(0) {
+  std::cout << "[ClapTrap] " << this->name << " constructed\n";
+}
+
+ClapTrap::~ClapTrap() {
+  std::cout << "[ClapTrap] " << name << " desstructed\n";
+}
